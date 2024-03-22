b839aca753c157ca3fe7402d295fdd8ee8593c3f
jop221 <mr.motomot@gmail.com>
2024-03-22 13:49:15 +0300

    fife

--- src/util.js
+++ src/util.js
@@ -1,10 +1,9 @@
 // src/cli.js
 import readlineSync from 'readline-sync';
+import start from './cli.js';
 const roundCount = 3;
 export function playGame (rules, generateRound) {
-    console.log('Welcome to the Brain Games!');
-  const userName = readlineSync.question('May I have your name? ');
-  console.log(`Hello, ${userName}!`);
+  console.log(start());
   console.log(rules); 
     for (let i = 0; i < roundCount; i += 1) {
       const [question, correctAnswer] = generateRound();
