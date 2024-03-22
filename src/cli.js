189ef1723c12f5e111cf5e499b6ab94943884474
jop221 <mr.motomot@gmail.com>
2024-03-22 15:04:17 +0300

    seven

--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,5 @@
 // src/cli.js
 import readlineSync from 'readline-sync';
-
 const start = () => {
     console.log('Welcome to the Brain Games!');
     const userName = readlineSync.question('May I have your name? ');
@@ -8,24 +7,4 @@ const start = () => {
   };
   
   export default start;
-export function getAnswer (num1 ,sign,num2) {
-switch (sign) {
-    case '+':
-      return num1 + num2;
-    case '-':
-      return num1 - num2;
-    case '*':
-      return num1 * num2;
-    default:
-      return null;
-  }
-};
-export function Roundcalc () {
-  const num1 = getRandomNumber(1, 100);
-  const num2 = getRandomNumber(1, 100);
-  const signs = ['+', '-', '*'];
-  const sign = signs[getRandomNumber(0, 2)];
-  const question = `${num1} ${sign} ${num2}`;
-  const answer = getAnswer(num1, sign, num2);
-  return [question, String(answer)];
-  };
+
