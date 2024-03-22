55ad8f1d9bf54f08bd0923f5fda05c803944cbb7
jop221 <mr.motomot@gmail.com>
2024-03-22 13:28:34 +0300

    fife

---
+++ bin/brain-even.js
@@ -0,0 +1,18 @@
+#!/usr/bin/env node
+import { playGame, getRandomNumber } from '../src/util.js';
+
+const rules = 'Answer "yes" if the number is even, otherwise answer "no".';
+
+const isEven = (num) => num % 2 === 0;
+
+const generateRound = () => {
+  const question = getRandomNumber(1, 100);
+  const correctAnswer = isEven(question) ? 'yes' : 'no';
+  return [question, correctAnswer];
+};
+
+const startBrainEven = () => {
+  playGame(rules, generateRound);
+};
+
+startBrainEven();
--- src/util.js
+++ src/util.js
@@ -1,17 +1,11 @@
 // src/cli.js
 import readlineSync from 'readline-sync';
-
-const start = () => {
-    console.log('Welcome to the Brain Games!');
-    const userName = readlineSync.question('May I have your name? ');
-    console.log(`Hello, ${userName}!`);
-  };
-  
-  export default start;
 const roundCount = 3;
 export function playGame (rules, generateRound) {
-    console.log(start());
-    console.log(rules); 
+    console.log('Welcome to the Brain Games!');
+  const userName = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${userName}!`);
+  console.log(rules); 
     for (let i = 0; i < roundCount; i += 1) {
       const [question, correctAnswer] = generateRound();
       console.log(`Question: ${question}`);
