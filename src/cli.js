a69469b8619b15cfcf54920345f216515bd29743
jop221 <mr.motomot@gmail.com>
2024-03-22 13:46:55 +0300

    fife

--- src/cli.js
+++ src/cli.js
@@ -8,23 +8,3 @@ const start = () => {
   };
   
   export default start;
-const roundCount = 3;
-export function playGame (rules, generateRound) {
-    console.log(start());
-    console.log(rules); 
-    for (let i = 0; i < roundCount; i += 1) {
-      const [question, correctAnswer] = generateRound();
-      console.log(`Question: ${question}`);
-      const userAnswer = readlineSync.question('Your answer: ');
-      if (userAnswer === correctAnswer) {
-        console.log('Correct!');
-      } else {
-        console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'\nLet's try again, ${userName}!`);
-        return;
-      }
-      if (i === roundCount - 1) {
-        console.log(`Congratulations, ${userName}!`);
-      }
-    }
-};
-export const getRandomNumber = (min, max) => Math.floor(Math.random() * (max - min + 1) + min);
