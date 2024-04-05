88d8dab4488b88b20b212138cdbcad00a160bb84
jop221 <mr.motomot@gmail.com>
2024-04-05 13:16:58 +0300

    feedback2

--- makefile
+++ makefile
@@ -1,23 +1,41 @@
 install: install-deps
-    npx simple-git-hooks
-run:    bin/nodejs-package.js 10
+	npx simple-git-hooks
+
+run:
+	bin/nodejs-package.js 10
+
 install-deps:
-    npm ci
-test:    npm test
+	npm ci
+
+test:
+	npm test
+
 test-coverage:
-    npm test -- --coverage --coverageProvider=v8    
-lint:    npx eslint  
+	npm test -- --coverage --coverageProvider=v8	
+
+lint:
+	npx eslint	.
+
 lint-fix:
-    npx eslint . --fix    
-publish:    npm publish --dry-run
+	npx eslint . --fix
+	
+publish:
+	npm publish --dry-run
+
 brain-games: 
-    node bin/brain-games.js
-brain-even:    node bin/brain-even.js  
+	node bin/brain-games.js
+
+brain-even:
+	node bin/brain-even.js	
+
 brain-calc:
-    node bin/brain-calc.js            
-brain-gcd:    node bin/brain-gcd.js
+	node bin/brain-calc.js
+			
+brain-gcd:
+	node bin/brain-gcd.js
+
 brain-progression:
-    node bin/brain-progression.js
-brain-prime:    node bin/brain-prime.js
-cli:
-    node /home/student161/frontend-project-44/src/cli.js
+	node bin/brain-progression.js
+
+brain-prime:
+	node bin/brain-prime.js	
---
+++ src/util.js
@@ -0,0 +1,98 @@
+// src/cli.js
+import readlineSync from 'readline-sync';
+const roundCount = 3;
+export function playGame (rules, generateRound) {
+  console.log('Welcome to the Brain Games!');
+  const userName = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${userName}!`);
+  console.log(rules); 
+    for (let i = 0; i < roundCount; i += 1) {
+      const [question, correctAnswer] = generateRound();
+      console.log(`Question: ${question}`);
+      const userAnswer = readlineSync.question('Your answer: ')
+      if (userAnswer === correctAnswer) { 
+        console.log('Correct!');
+      } else {
+        console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'\nLet's try again, ${userName}!`);
+        return;
+      }
+      if (i === roundCount - 1) {
+        console.log(`Congratulations, ${userName}!`);
+      }
+    }
+};
+const isEven = (num) => num % 2 === 0;
+
+export const generateRound = () => {
+  const question = getRandomNumber(1, 100);
+  const correctAnswer = isEven(question) ? 'yes' : 'no';
+  return [question, correctAnswer];
+};
+export const getRandomNumber = (min, max) => Math.floor(Math.random() * (max - min + 1) + min);
+export function getAnswer (num1 ,sign,num2) {
+  switch (sign) {
+      case '+':
+        return num1 + num2;
+      case '-':
+        return num1 - num2;
+      case '*':
+        return num1 * num2;
+      default:
+        return null;
+    }
+  };
+  export function Roundcalc () {
+    const num1 = getRandomNumber(1, 100);
+    const num2 = getRandomNumber(1, 100);
+    const signs = ['+', '-', '*'];
+    const sign = signs[getRandomNumber(0, 2)];
+    const question = `${num1} ${sign} ${num2}`;
+    const answer = getAnswer(num1, sign, num2);
+    return [question, String(answer)];
+  };
+  function  calculateGCD (num1, num2) { 
+    if (num2 > num1) return calculateGCD(num2, num1);
+    if (!num2) return num1;
+    return calculateGCD(num2, num1 % num2);
+  };
+  export function RoundGCD () {
+    const num1 = getRandomNumber(1, 50);
+    const num2 = getRandomNumber(1, 50);
+    const gcd = calculateGCD(num1,num2);
+    const question = `${num1} ${num2}`;
+    const answer = String(gcd);
+    return [question, answer];
+  };
+  const generateProgression = (randomFirstNum, minLength, step) => {
+    const resultProgression = [];
+    for (let i = randomFirstNum; i < minLength; i += step) {
+      if (resultProgression.length !== 10) resultProgression.push(i);
+    }
+    return resultProgression;
+  };
+  export function Roundprodression () {
+    const randomFirstNum = getRandomNumber(1, 10);
+    const step = getRandomNumber(1, 5);
+    const minLength = randomFirstNum + (step * 10);
+  
+    const progression = generateProgression(randomFirstNum, minLength, step);
+    const hiddenIndex = getRandomNumber(0, progression.length - 1);
+    const correctAnswer = progression[hiddenIndex].toString();
+    progression[hiddenIndex] = '..';
+    const question = progression.join(' ');
+  
+    return [question, correctAnswer];
+  };
+  const isPrime = (num) => {
+    if (num === 0 || num === 1) return false;
+    for (let i = 2; i < num; i += 1) {
+      if (num % i === 0) return false;
+    }
+    return true;
+  };
+  export function Roundprime () {
+    const num = getRandomNumber(1, 100);
+    const correctAnswer = isPrime(num) ? 'yes' : 'no';
+    const question = String(num);
+    return [question, correctAnswe];
+  };
